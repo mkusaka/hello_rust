858f76353317be36bb825463af798af6f3a1397a
mkusaka <hinoshita1992@gmail.com>
2019-05-03 01:18:58 +0900

    feat: add first test and fix index

--- ch03/bitonic-sorter/src/first.rs
+++ ch03/bitonic-sorter/src/first.rs
@@ -19,8 +19,41 @@ fn sub_sort(x: &mut [u32], up: bool) {
 fn compare_and_swap(x: &mut [u32], up: bool) {
   let mid_point = x.len() / 2;
   for i in 0..mid_point {
-    if (x[i] > x [mid_point + 1]) == up {
-      x.swap(i, mid_point + 1);
+    if (x[i] > x[mid_point + i]) == up {
+      x.swap(i, mid_point + i);
     }
+  }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+  use super::sort;
+
+  #[test]
+  fn sort_u_32_ascending() {
+    let mut x = vec![10, 30, 11, 20, 4, 330, 220, 4440, 10, 1, 444, 4423, 8236, 23, 324, 76];
+
+    sort(&mut x, true);
+
+    assert_eq!(x, vec![1, 4, 10, 10, 11, 20, 23, 30, 76, 220, 324, 330, 444, 4423, 4440, 8236]);
+  }
+
+  #[test]
+  fn sort_u_32_descending() {
+    let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+
+    sort(&mut x, false);
+
+    assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+  }
+
+  #[test]
+  fn sort_u32_descendingorg() {
+    let mut x = vec![10, 30, 11, 20, 4, 330, 21, 110];
+    sort(&mut x, false);
+    // xの要素が降順にソートされていることを確認する
+    assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
   }
 }
