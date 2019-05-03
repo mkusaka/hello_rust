088fc29c3c5470bf50c5ac1e70b94694220930e8
mkusaka <hinoshita1992@gmail.com>
2019-05-03 18:50:31 +0900

    fixme: add error implement

--- ch03/bitonic-sorter/src/third.rs
+++ ch03/bitonic-sorter/src/third.rs
@@ -1,18 +1,69 @@
 use std::cmp::Ordering;
+use super::SortOrder;
+
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+  if x.len().is_power_of_two() {
+    match *order {
+      SortOrder::Ascending => sort_by(x, &|a, b| a.cmp(b)),
+      SortOrder::Descending => sort_by(x, &|a, b| b.cmp(a)),
+    };
+    Ok(())
+  } else {
+    Err(format!("The length of x is not expected. it must be the power of 2. current length is {}", x.len()))
+  }
+}
 
 pub fn sort_by<T, F>(x: &mut [T], comparator: &F) -> Result<(), String>
   where F: Fn(&T, &T) -> Ordering
 {
-  if is_power_of_two(x.len()) {
-    do_sort(x, true, comparatore);
+  if x.len().is_power_of_two() {
+    do_sort(x, true, comparator);
     Ok(())
   } else {
     Err(format!("The length of x is not a powe of two. {}", x.len()))
   }
 }
 
+fn do_sort<T: Ord, F>(x: &mut [T], forward: bool, comparator: &F)
+  where F: Fn(&T, &T) -> Ordering
+{
+  if x.len() > 1 {
+    let mid_point = x.len() / 2;
+    do_sort(&mut x[..mid_point], true, comparator);
+    do_sort(&mut x[mid_point..], false, comparator);
+    sub_sort(x, forward, comparator)
+  }
+}
+
+fn sub_sort<T: Ord, F>(x: &mut [T], forward: bool, comparator: &F)
+  where F: Fn(&T, &T) -> Ordering
+{
+  if x.len() > 1 {
+    compare_and_swap(x, forward, comparator);
+    let mid_point = x.len() / 2;
+    sub_sort(&mut x[..mid_point], forward, comparator);
+    sub_sort(&mut x[mid_point..], forward, comparator);
+  }
+}
+
+fn compare_and_swap<T: Ord, F>(x: &mut [T], forward: bool, compare_and_swap: &F)
+  where F: Fn(&T, &T) -> Ordering
+{
+  let swap_condition = if forward {
+    Ordering::Greater
+  } else {
+    Ordering::Less
+  };
+  let mid_point = x.len() / 2;
+  for i in 0..mid_point {
+    if (x[i] > x[mid_point + i]) == swap_condition {
+      x.swap(i, mid_point + i);
+    }
+  }
+}
+
 mod tests {
-  use super::{is_power_of_two, sort, sort_by};
+  use super::{sort, sort_by};
   use crate::SortOrder::*;
 
   struct Student {
@@ -64,7 +115,7 @@ mod tests {
 
     assert_eq!(
       sort_by(&mut x, &|a, b|
-        a.last_name.cmp(&b.last_name).then_with(|| a.frist_name.cmp(&b.first_name))
+        a.last_name.cmp(&b.last_name).then_with(|| a.first_name.cmp(&b.first_name))
       ),
       Ok(())
     );
