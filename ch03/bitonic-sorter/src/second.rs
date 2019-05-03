a30fdb3ecf01d89a68ca625474751b287d91fcdb
mkusaka <hinoshita1992@gmail.com>
2019-05-03 14:22:23 +0900

    feat: init second sort

---
+++ ch03/bitonic-sorter/src/second.rs
@@ -0,0 +1,67 @@
+pub fn sort(x: &mut [u32], up: bool) {
+  if x.len() > 1 {
+    let mid_point = x.len() / 2;
+    sort(&mut x[..mid_point], true);
+    sort(&mut x[mid_point..], false);
+    sub_sort(x, up)
+  }
+}
+
+fn sub_sort(x: &mut [u32], up: bool) {
+  if x.len() > 1 {
+    compare_and_swap(x, up);
+    let mid_point = x.len() / 2;
+    sub_sort(&mut x[..mid_point], up);
+    sub_sort(&mut x[mid_point..], up);
+  }
+}
+
+fn compare_and_swap(x: &mut [u32], up: bool) {
+  let mid_point = x.len() / 2;
+  for i in 0..mid_point {
+    if (x[i] > x[mid_point + i]) == up {
+      x.swap(i, mid_point + i);
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::sort;
+
+  #[test]
+  fn sort_u_32_ascending() {
+    let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 220, 4440, 10, 1, 444, 4423, 8236, 23, 324, 76];
+
+    sort(&mut x, true);
+
+    assert_eq!(x, vec![1, 4, 10, 10, 11, 20, 23, 30, 76, 220, 324, 330, 444, 4423, 4440, 8236]);
+  }
+
+  #[test]
+  fn sort_u_32_descending() {
+    let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
+
+    sort(&mut x, false);
+
+    assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
+  }
+
+  #[test]
+  fn sort_str_ascending() {
+    let mut x = vec!["Rust", "is"];
+
+    sort(&mut x, true);
+
+    assert_eq!(x, vec!["Rust", "is"]);
+  }
+
+  #[test]
+  fn sort_str_descending() {
+    let mut x = vec!["Rust", "is"];
+
+    sort(&mut x, false);
+
+    assert_eq!(x, vec!["is", "Rust"]);
+  }
+}
