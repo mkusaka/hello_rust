ca9bdc71f015549b3b26467a05eac5903e462f8b
mkusaka <hinoshita1992@gmail.com>
2019-05-12 14:40:47 +0900

    :+1: feat: add Loc, Annot, Token

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Loc(usize, usize);
+
+
+impl Loc {
+    fn merge(&self, other, &Loc) -> {
+        use std::cmp::{max, min};
+        Loc(min(self.0, other.0), max(self.1, ohter.1))
+    }
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+
+struct Annot<T> {
+    value: T,
+    loc: Loc,
+}
+
+impl<T> Annot<T> {
+    fn new(value: T, loc: Loc) -> Self {
+        Self { value, loc }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum TokenKind {
+    Number(u64),
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    LParen,
+    RParen
+}
+
+type Token = Annot<TokenKind>
